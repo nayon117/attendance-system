b1c42d02fe3c14d9bce59ecb49d82bc6ccbcbbb3
nayon117 <hasibul.nayon1@gmail.com>
2024-11-14 21:14:49 +0600

    implement get disable func

--- controllers/admin-attendance.js
+++ controllers/admin-attendance.js
@@ -1,7 +1,8 @@
+const { addMinutes, isAfter } = require("date-fns");
 const AdminAttendance = require("../models/AdminAttendence");
 const error = require("../utils/error");
 
-const getEnable =async (req,res,next) =>{
+const getEnable =async (_req,res,next) =>{
   try {
     const running = await AdminAttendance.findOne({status:'RUNNING'});
     if(running){
@@ -14,8 +15,40 @@ const getEnable =async (req,res,next) =>{
     next(error)
   }
 }
-const getDisable = (req,res,next) =>{}
+
+const getStatus = async(_req,res,next) =>{
+  try {
+    const running = await AdminAttendance.findOne({status:'RUNNING'});
+    if(!running){
+      throw error('Not running',400);
+    }
+    const started = addMinutes(new Date(running.createdAt),running.timelimit);
+    if(isAfter(new Date(),started)){
+      running.status = "COMPLETED";
+      await running.save();
+    }
+    return res.status(200).json(running);
+  } catch (error) {
+    next(error)
+  }
+}
+
+const getDisable = async(_req,res,next) =>{
+  try {
+    const running = await AdminAttendance.findOne({status:'RUNNING'});
+    if(!running){
+      throw error('Not running',400);
+    }
+
+      running.status = "COMPLETED";
+      await running.save();
+
+     return res.status(200).json(running);
+  } catch (error) {
+    next(error)
+  }
+}
 
 module.exports = {
-  getEnable,getDisable
+  getEnable,getDisable,getStatus
 }
--- routes/admin-attendance.js
+++ routes/admin-attendance.js
@@ -3,5 +3,6 @@ const adminAttendance = require('../controllers/admin-attendance');
 
 router.get('/enable',adminAttendance.getEnable);
 router.get('/disable',adminAttendance.getDisable);
+router.get('/status',adminAttendance.getStatus);
 
 module.exports = router;
